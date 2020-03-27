ecc8bce15c4a960e70bde72331a7d1622cc0950a
Ryomasao <konoemario@gmail.com>
2020-03-27 19:03:49 +0900

    自己紹介ページをつくることにする

--- hello-world/src/pages/about.js
+++ hello-world/src/pages/about.js
@@ -1,17 +1,63 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
+/** @jsx jsx */
+import { jsx, css } from "@emotion/core"
+import Layout from "../components/Layout"
+
+import CombatPower from "../components/CombatPower"
 
 export default ({ data }) => {
   return (
-    <div>
-      <h1>{data.site.siteMetadata.title}</h1>
-      <div>
-        <Link to="/">back to Top using link</Link>
+    <Layout>
+      <div css={{ width: "90%", margin: "50px auto 0" }}>
+        <div css={{ display: "flex", bolder: "1px solid #f3f3f3" }}>
+          <div css={{ width: "60%" }}>
+            <div>
+              <h4>汎用機(Z/OS) + JCL + COBOLによるバックエンドの開発(3年)</h4>
+              <p>
+                開発環境は(今思えば)ものすごくレガシーだった。
+                でも当時の開発体制は、がちがちのウォータフォールで、工程管理や成果物管理、テスト手法がとても厳格だった。
+                生産性という観点ではとても悪かったけれども、大規模開発に向く開発手法を学ぶことができた。
+              </p>
+              <h4>よかった点</h4>
+              <p>ああああああ</p>
+            </div>
+          </div>
+          <div css={{ width: "40%" }}>
+            <CombatPower
+              front={0}
+              backend={0}
+              devOps={0}
+              analyze={4}
+              teamWork={4}
+            />
+          </div>
+        </div>
+        <div css={{ display: "flex", bolder: "1px solid #f3f3f3" }}>
+          <div css={{ width: "60%" }}>
+            <div>
+              <h4>汎用機(Z/OS) + JCL + COBOLによるバックエンドの開発(3年)</h4>
+              <p>
+                開発環境は(今思えば)ものすごくレガシーだった。
+                でも当時の開発体制は、がちがちのウォータフォールで、工程管理や成果物管理、テスト手法がとても厳格だった。
+                生産性という観点ではとても悪かったけれども、大規模開発に向く開発手法を学ぶことができた。
+              </p>
+              <h4>よかった点</h4>
+              <p>ああああああ</p>
+            </div>
+          </div>
+          <div css={{ width: "40%" }}>
+            <CombatPower
+              front={0}
+              backend={0}
+              devOps={0}
+              analyze={4}
+              teamWork={4}
+            />
+          </div>
+        </div>
       </div>
-      <div>
-        <a href="/about"> cack to Top using a tag</a>
-      </div>
-    </div>
+    </Layout>
   )
 }
 
